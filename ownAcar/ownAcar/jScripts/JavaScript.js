5faa6e3d972656de3c89c5cd286c1ac5f0c1c0e6
Talrevivo <37411352+Talrevivo@users.noreply.github.com>
2020-03-19 14:05:40 +0200

    Add files via upload

---
+++ ownAcar/ownAcar/jScripts/JavaScript.js
@@ -0,0 +1,55 @@
+﻿//$(".js-orange").click(function () {
+//    $(".js-orange").css('color', 'green');
+
+//});
+
+/////אזהרות/
+
+//$(".openparhagraph").click(function () {
+//    $(".openparhagraph").css('background', 'white');
+
+//});
+//function openAcc(event) {
+
+//    var panel = document.getElementsByClassName("accordion");
+//    var i;
+//    for (i = 0; i < acc.length; i++) {
+//    acc[i].addEventListener("click", function () {
+//        this.classList.toggle("active");
+//        var panel = this.nextElementSibling;
+//    //console.log($(this).find(".panel").css("display"));
+//    //if ($(this).find(".panel").css("display") == "block") {
+//    //    Console.log($(this).find(".panel"));
+//    //    $(this).find(".panel").hide();
+//    //}
+//    //else {
+//    //    $(this).find(".panel").show();
+//    //}
+
+//    if (panel.style.display == "block") {
+
+//        panel.style.display = "none";
+//            } else {
+//                alert("in");
+//                panel.style.display = "block";
+//            }
+//    });
+//}
+
+
+//function openAcc() {
+//    var acc = document.getElementsByClassName("accordion");
+   
+//    for (var i = 0; i < acc.length; i++) {
+//        acc[i].addEventListener("click", function () {
+//            this.classList.toggle("active"); 
+//            var panel = this.nextElementSibling;
+//            if (panel.style.display == "block") {
+//                panel.style.display = "none";
+//            } else {
+//                panel.style.display = "block";
+//            }
+//        });
+//    }
+//} 
+
---
+++ ownAcar/ownAcar/jScripts/‏‏JavaScript2.js
@@ -0,0 +1,37 @@
+﻿///אקורדיון/
+
+$(document).ready(function () {
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        });
+    }
+});
+
+function tab_nav() {
+    var navA = document.querySelectorAll("ul li>a");
+    var navInner = document.querySelectorAll("li ul li>a");
+
+    for (var i = 0, len = navA.length; i < len; i++) {
+        navA[i].addEventListener("focus", function (event) {
+            var innerUl = event.target.parentElement.querySelector("ul")
+            if (innerUl) innerUl.style.display = "block";
+        }, true);
+    }
+
+    for (var i = 0, len = navInner.length; i < len; i++) {
+        if (navInner[i].parentElement.nextElementSibling === null) {
+            navInner[i].addEventListener("blur", function (event) {
+                event.target.parentElement.parentElement.style.display = "none"
+            }, true);
+        }
+    }
+}
